8362f070c5dd7284a9dbd705bcb14da3fa3afdff
anshnagrath <anshnagrath448@gmail.com>
2019-10-16 22:42:16 +0530

    bugs removed

--- src/components/PlaceDetail/PlaceDetails.js
+++ src/components/PlaceDetail/PlaceDetails.js
@@ -8,12 +8,12 @@ if(props.selectedPlace){
     modalContent = (
         <View>
             <Image source={props.selectedPlace.image} style={styles.placeImage}></Image>
-            <Text style={styles.placeName}>{props.selectedPlace.name}</Text>
+            <Text style={styles.placeName}>{props.selectedPlace.value}</Text>
         </View>
     )
 }
 return(
-    <Modal visible={props.selectedPlace !== null} animationType="slide">
+    <Modal onRequestClose={props.onItemClose} visible={props.selectedPlace !== null} animationType="slide">
         <View style={styles.modalContainer}>
             {modalContent}
             <Button style={styles.buttons} color="red" title="Delete" onPress={props.onItemDeleted}></Button>
@@ -27,14 +27,16 @@ const styles = StyleSheet.create({
     modalContainer:{
         margin:40
     },buttons:{
-        paddingBottom:10
+        marginBottom:50,
+        margin:10
     },placeImage:{
         width:"100%",
         height:200
     },placeName:{
         fontWeight:"bold",
+        padding:20,
         textAlign:"center",
-        fontSize:28
+        fontSize:20
     }
 
 })
